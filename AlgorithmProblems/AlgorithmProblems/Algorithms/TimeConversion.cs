7f7e42e0dbae6a355b8ce888a8b84147df6ee667
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2023-01-24 19:27:21 +0100

    Time Conversion Challenge
    Time Conversion Challenge

---
+++ AlgorithmProblems/AlgorithmProblems/Algorithms/TimeConversion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems.Algorithms
+{
+    internal class TimeConversion
+    {
+        public static string Execute(string s) 
+        {
+            var clock = s.Substring(s.Length - 2, 2);
+            var hour = s.Substring(0, s.Length - 2);
+
+            var time = hour.Split(":");
+
+            var hour_i = int.Parse(time[0]);
+
+            if (hour_i != 12)
+            {
+                if (clock == "PM") hour_i += 12;
+            }
+            else
+            {
+                if (clock == "AM") hour_i = 0;
+            }
+
+            return $"{hour_i.ToString().PadLeft(2, '0')}:{time[1]}:{time[2]}";
+        }
+    }
+}
---
+++ AlgorithmProblems/AlgorithmProblems/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmProblems.Algorithms;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(TimeConversion.Execute("01:05:45AM"));
+
+Console.ReadLine();
